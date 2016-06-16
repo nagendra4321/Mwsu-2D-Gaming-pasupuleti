02e992b030a1858101a7c34412a9f82edba58de6
nagendrababuus <nagendrababuus@gmail.com>
2016-06-16 13:02:30 -0500

    Add files via upload

---
+++ Program-3/js/menu.js
@@ -0,0 +1,41 @@
+var menuState = {
+
+    create: function() { 
+        game.add.image(0, 0, 'background');
+
+        if (!localStorage.getItem('bestScore')) {
+            localStorage.setItem('bestScore', 0);
+        }
+
+        if (game.global.score > localStorage.getItem('bestScore')) {
+            localStorage.setItem('bestScore', game.global.score);
+        }
+
+        var nameLabel = game.add.text(game.width/2, -50, 'Packman', { font: '70px Geo', fill: '#ffffff' });
+        nameLabel.anchor.setTo(0.5, 0.5);
+        game.add.tween(nameLabel).to({y: 80}, 1000).easing(Phaser.Easing.Bounce.Out).start();
+
+        var text = 'score: ' + game.global.score + '\nbest score: ' + localStorage.getItem('bestScore');
+        var scoreLabel = game.add.text(game.width/2, game.height/2, text, { font: '25px Arial', fill: '#ffffff', align: 'center' });
+        scoreLabel.anchor.setTo(0.5, 0.5);
+
+        var startLabel = game.add.text(game.width/2, game.height-80, 'press the up arrow key to start', { font: '25px Arial', fill: '#ffffff' });
+        startLabel.anchor.setTo(0.5, 0.5);
+        game.add.tween(startLabel).to({angle: -2}, 500).to({angle: 2}, 1000).to({angle: 0}, 500).loop().start(); 
+
+        var upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+        upKey.onDown.add(this.start, this);
+
+        this.muteButton = game.add.button(20, 20, 'mute', this.toggleSound, this);
+        this.muteButton.frame = game.sound.mute ? 1 : 0;
+    },
+
+    toggleSound: function() {
+        game.sound.mute = !game.sound.mute;
+        this.muteButton.frame = game.sound.mute ? 1 : 0;
+    },
+
+    start: function() {
+        game.state.start('play');   
+    },
+};
---
+++ Program-3/js/play.js
@@ -0,0 +1,171 @@
+var playState = {
+
+    create: function() {
+        
+        this.cursor = game.input.keyboard.createCursorKeys();
+        game.input.keyboard.addKeyCapture([Phaser.Keyboard.UP, Phaser.Keyboard.DOWN, Phaser.Keyboard.LEFT, Phaser.Keyboard.RIGHT]);
+        this.wasd = {
+            up: game.input.keyboard.addKey(Phaser.Keyboard.W),
+            left: game.input.keyboard.addKey(Phaser.Keyboard.A),
+            right: game.input.keyboard.addKey(Phaser.Keyboard.D)
+        };
+
+        this.player = game.add.sprite(game.width/2, game.height/2, 'player');
+        this.player.anchor.setTo(0.5, 0.5);
+        game.physics.arcade.enable(this.player);
+        this.player.body.gravity.y = 500;
+        this.player.animations.add('right');
+        this.player.animations.add('left');
+
+        this.createWorld();
+
+        this.coin = game.add.sprite(60, 140, 'coin');
+        game.physics.arcade.enable(this.coin); 
+        this.coin.anchor.setTo(0.5, 0.5);
+
+        this.scoreLabel = game.add.text(30, 30, 'score: 0', 
+            { font: '18px Arial', fill: '#ffffff' });
+        game.global.score = 0; 
+
+        this.enemies = game.add.group();
+        this.enemies.enableBody = true;
+        this.enemies.createMultiple(10, 'enemy');
+        this.nextEnemy = 0;
+
+        this.jumpSound = game.add.audio('jump');
+        this.coinSound = game.add.audio('coin');
+        this.deadSound = game.add.audio('dead'); 
+
+        this.emitter = game.add.emitter(0, 0, 15);
+        this.emitter.makeParticles('pixel');
+        this.emitter.setYSpeed(-150, 150);
+        this.emitter.setXSpeed(-150, 150);
+        this.emitter.setScale(2, 0, 2, 0, 800);
+        this.emitter.gravity = 0;
+    },
+
+    update: function() {
+        game.physics.arcade.collide(this.player, this.layer);
+        game.physics.arcade.collide(this.enemies, this.layer);
+        game.physics.arcade.overlap(this.player, this.coin, this.takeCoin, null, this);
+        game.physics.arcade.overlap(this.player, this.enemies, this.playerDie, null, this);
+
+        if (!this.player.alive) {
+            return;
+        }
+        
+        this.movePlayer(); 
+
+        if (!this.player.inWorld) {
+            this.playerDie();
+        }
+
+        if (this.nextEnemy < game.time.now) {
+            var start = 4000, end = 1000, score = 100;
+            var delay = Math.max(start - (start - end) * game.global.score / score, end);
+            
+            this.addEnemy();
+            this.nextEnemy = game.time.now + delay;
+        }
+    },
+
+    movePlayer: function() {
+        if (this.cursor.left.isDown || this.wasd.left.isDown) {
+            this.player.body.velocity.x = -200;
+            this.player.scale.x=-1;
+            this.player.animations.play('left');
+        }
+        else if (this.cursor.right.isDown || this.wasd.right.isDown) {
+            this.player.body.velocity.x = 200;
+            this.player.scale.x=1;
+            this.player.animations.play('right');
+        }
+        else {
+            this.player.body.velocity.x = 0;
+            this.player.animations.stop(); 
+            this.player.frame = 0; 
+        }
+        
+        if ((this.cursor.up.isDown || this.wasd.up.isDown) && this.player.body.onFloor()) {
+            this.jumpSound.play();
+            this.player.body.velocity.y = -320;
+        }
+    },
+
+    takeCoin: function(player, coin) {
+        game.global.score += 5;
+        this.scoreLabel.text = 'score: ' + game.global.score;
+        
+        this.updateCoinPosition();
+
+        this.coinSound.play();
+        this.coin.scale.setTo(0, 0);
+        game.add.tween(this.coin.scale).to({x: 1, y: 1}, 300).start();
+        game.add.tween(this.player.scale).to({x: 1.3, y: 1.3}, 100).yoyo(true).start();
+    },
+
+    updateCoinPosition: function() {
+        var coinPosition = [
+            {x: 140, y: 60}, {x: 360, y: 60}, 
+            {x: 60, y: 140}, {x: 440, y: 140}, 
+            {x: 130, y: 300}, {x: 370, y: 300} 
+        ];
+
+        for (var i = 0; i < coinPosition.length; i++) {
+            if (coinPosition[i].x == this.coin.x) {
+                coinPosition.splice(i, 1);
+            }
+        }
+
+        var newPosition = game.rnd.pick(coinPosition);
+        this.coin.reset(newPosition.x, newPosition.y);
+    },
+
+    addEnemy: function() {
+        var enemy = this.enemies.getFirstDead();
+
+        if (!enemy) {
+            return;
+        }
+
+        enemy.anchor.setTo(0.5, 1);
+        enemy.reset(game.width/2, 0);
+        enemy.body.gravity.y = 500;
+        enemy.body.velocity.x = 100 * game.rnd.pick([-1, 1]);
+        enemy.body.bounce.x = 1;
+        enemy.checkWorldBounds = true;
+        enemy.outOfBoundsKill = true;
+    },
+
+    createWorld: function() {
+            // Create the tilemap
+        this.map = game.add.tilemap('map');
+
+        // Add the tileset to the map
+        this.map.addTilesetImage('tileset');
+
+        // Create the layer by specifying the name of the Tiled layer
+        this.layer = this.map.createLayer('Tile Layer 1');
+
+        // Set the world size to match the size of the layer
+        this.layer.resizeWorld();
+
+        // Enable collisions for the first tilset element (the blue wall)
+        this.map.setCollision(1);
+        },
+
+    playerDie: function() {
+        this.player.kill();
+        
+        this.deadSound.play();
+        this.emitter.x = this.player.x;
+        this.emitter.y = this.player.y;
+        this.emitter.start(true, 800, null, 15);
+        game.time.events.add(1000, this.startMenu, this);
+        game.camera.shake(0.02, 300);
+    },
+
+    startMenu: function() {
+        game.state.start('menu');
+    },
+};
